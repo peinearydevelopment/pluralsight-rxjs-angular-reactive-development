4b9c0693be4b0d64c83860e163372a1a7021fd44
Peineary Development <peinearydevelopment@gmail.com>
2019-07-25 16:18:17 -0400

    mapping returned data

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -18,11 +18,17 @@ export class ProductService {
   constructor(private http: HttpClient,
               private supplierService: SupplierService) { }
 
-  products$ = this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
+  products$ = this.http
+                  .get<Product[]>(this.productsUrl)
+                  .pipe(
+                    tap(data => console.log('Products: ', JSON.stringify(data))),
+                    map(products => products.map(product => ({
+                      ...product,
+                      price: product.price * 1.5,
+                      searchKey: [product.productName]
+                    }) as Product)),
+                    catchError(this.handleError)
+                  );
 
   private fakeProduct() {
     return {
